d01c5b2c30a36a2c5270d7e19b5fe76294cc315a
RayaneBengaoui <rayane.bengaoui@gmail.com>
2021-05-16 09:39:50 +0200

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { HeaderComponent } from './todo-edit/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoEditComponent,
+    TodoListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
