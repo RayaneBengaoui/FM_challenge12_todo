a16fad7ae08855820f34bf14f571f6ef9366b37d
RayaneBengaoui <rayane.bengaoui@gmail.com>
2021-05-17 21:15:24 +0200

    Modifying the icon from the theme toggle on click

--- src/app/components/todos/todo-edit/header/header.component.ts
+++ src/app/components/todos/todo-edit/header/header.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('bgImage') bgImage!: ElementRef;
 
-  constructor() { }
+  backgroundImages = {
+    desktop: {
+      light: {
+        path: 'assets/icon-sun.svg',
+        description: 'sun',
+      },
+      dark: {
+        path: 'assets/icon-moon.svg',
+        description: 'moon',
+      },
+    },
+  };
 
-  ngOnInit(): void {
-  }
+  onThemeChange = (): void => {
+    const bgPath = this.bgImage.nativeElement.alt;
 
+    if (bgPath === 'moon') {
+      this.bgImage.nativeElement.src = this.backgroundImages.desktop.light.path;
+      this.bgImage.nativeElement.alt =
+        this.backgroundImages.desktop.light.description;
+    } else {
+      this.bgImage.nativeElement.src = this.backgroundImages.desktop.dark.path;
+      this.bgImage.nativeElement.alt =
+        this.backgroundImages.desktop.dark.description;
+    }
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
